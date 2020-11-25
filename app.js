6164d4419d89c16f01a9135bb1c05a492dd2820c
etheriuman <timmy26291125@gmail.com>
2020-11-25 22:39:02 +0800

    add log request time function

--- app.js
+++ app.js
@@ -3,6 +3,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+
+// middleware
+app.use( function (req, res, next) {
+  // 請求的url
+  const url = req.originalUrl
+  // 請求的方法
+  const method = req.method
+
+  // 請求的時間
+  const time =  new Date(Date.now())
+
+  const YYYY = time.getFullYear()
+  const MM = time.getMonth()
+  const DD = time.getDate()
+  const hr = time.getHours()
+  const min = time.getMinutes()
+  const sec = time.getSeconds()
+
+  // 輸出字串
+  const timeOutPrint = `${YYYY}-${MM}-${DD} ${hr}:${min}:${sec}`
+
+  // log
+  console.log(`${timeOutPrint} | ${method} from '${url}'`)
+  next()
+})
+
+
+
+// 路由設定
+
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
 })
